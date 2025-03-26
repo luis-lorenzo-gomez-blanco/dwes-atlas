72787221af6e8851910d22094daaab4fbbd9c348
Luis Lorenzo Gómez Blanco <luis.gomezblanco@educa.madrid.org>
2025-03-26 12:21:48 +0100

    Add files via upload

--- atlas.sql
+++ atlas.sql
@@ -1,15 +1,18 @@
+-- Tabla de usuarios
 CREATE DATABASE IF NOT EXISTS dwes;
 USE dwes;
-
-DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS usuarios;
 CREATE TABLE usuario (
 	nombre VARCHAR(50),
 	password VARCHAR(25),
 	CONSTRAINT pk_usuario PRIMARY KEY (nombre)
 );
-INSERT INTO usuario VALUES
+INSERT INTO usuarios VALUES
 ('admin','atlas'),('usuario','usuario');
 
+-- Tabla de atlas
+CREATE DATABASE IF NOT EXISTS dwes;
+USE dwes;
 DROP TABLE IF EXISTS atlas;
 CREATE TABLE atlas(
 	pais VARCHAR(50),
@@ -18,3 +21,4 @@ CREATE TABLE atlas(
 );
 INSERT INTO atlas
 VALUES ('Francia','París'), ('Italia','Roma');
+
--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-session_start();
-$_SESSION['msg_info'] = "";
 require_once './src/modelo.php';
 require_once './src/vista.php';
 require_once './src/controlador.php';
-
-// Inicializamos la variable de acción para evitar errores si no existe
-if (!isset($_POST['accion'])) $opcion[0] = ''; else $opcion = array_keys($_POST['accion']);
-
-// En $opcion[0] está almacenada la acción elegida por el usuario
-
-switch ($opcion[0]) {
-	// Registro de usuario en registro.html
-	case "registro_registrarse":
-		Controlador::usuarioRegistro();
-		break;
-	// Alta de usuario en registro.html
-	case "registro_alta_de_usuario":
-		Controlador::usuarioAlta();
-		break;
-	default:
-		Controlador::iniciar();
-}
-
 ?>
